b748fd2b0be11749628a7007baf808effa0efda9
ptim <ptim@orcd-login003.mit.edu>
2026-03-02 13:55:14 -0500

    Added ROCm

--- run_FlowER_large_newData.sh
+++ run_FlowER_large_newData.sh
@@ -8,7 +8,7 @@ export RBF_HIGH=12
 export RBF_GAP=0.1
 export SIGMA=0.15
 
-export MODEL_NAME="model.240000_7.pt" # your trained checkpoint here
+export MODEL_NAME="" # your trained checkpoint here
 
 export TRAIN_BATCH_SIZE=4096
 export VAL_BATCH_SIZE=4096
@@ -16,9 +16,10 @@ export TEST_BATCH_SIZE=1024
 
 export PARSE_WORKERS=4
 export NUM_WORKERS=4
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0
+#export HIP_VISIBLE_DEVICES=0
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-export NUM_GPUS_PER_NODE=2
+export NUM_GPUS_PER_NODE=1
 
 export NUM_NODES=1
 export NODE_RANK=0
@@ -37,18 +38,18 @@ export TEST_FILE=${TEST_FILE:-/home/ptim/orcd/scratch/data/FlowERrs_test_200.txt
 export MODEL_PATH=/home/ptim/orcd/scratch/FlowERrs_checkpoints/$DATA_NAME/$EXP_NAME/
 #export RESULT_PATH=/home/ptim/orcd/scratch/FlowERrs_results/$DATA_NAME/$EXP_NAME/mit_preemptable/
 
-export RESULT_PATH=/home/ptim/orcd/scratch/FlowERrs_results/$DATA_NAME/$EXP_NAME/mit_preemptable/quicktest/tmp/
+#export RESULT_PATH=/home/ptim/orcd/scratch/FlowERrs_results/$DATA_NAME/$EXP_NAME/mit_preemptable/quicktest/tmp/
 
  [ -f $TRAIN_FILE ] || { echo $TRAIN_FILE does not exist; exit; }
  [ -f $VAL_FILE ] || { echo $VAL_FILE does not exist; exit; }
  [ -f $TEST_FILE ] || { echo $TEST_FILE does not exist; exit; }
 
 
-#export SCALE=4 # smaller sample size during training validation
-#sh /home/ptim/FlowER/FlowERrs/scripts/train.sh
+export SCALE=4 # smaller sample size during training validation
+sh /home/ptim/FlowER/FlowERrs/scripts/train.sh
 
-export SCALE=1 # larger sample size during testing
+#export SCALE=1 # larger sample size during testing
 
-sh scripts/eval_multiGPU.sh
+#sh scripts/eval_multiGPU.sh
 # sh scripts/search.sh
 # sh scripts/search_multiGPU.sh
